2c2a970aa9fc4087cbe82c1caa239af9ddcc48b3
hulugen-wannabe <bsha44893@gmail.com>
2022-11-23 20:13:44 +0800

    you forgot to do this

---
+++ src/component/Header1.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const Header1 = () => {
+  return (
+    <div id="Header">
+      <div id="text">
+        <div id="num1">
+          <img
+            src="/image/logo4.png"
+            alt="Netflix logo"
+            width={"120px"}
+            height={"100px"}
+          />
+          <button id="btn1">Home</button>
+          <button id="btn2">Series</button>
+          <button id="btn2">Films</button>
+          <button id="btn2">Latest</button>
+          <button id="btn2">My List</button>
+          <button id="btn2">Browse by Languages</button>
+        </div>
+        <div id="num2">
+          <a href="rougn">
+            <img
+              src="https://www.pngfind.com/pngs/b/54-545056_search-icon-png.png"
+              width={"25px"}
+              height={"25px"}
+            ></img>
+          </a>
+        </div>
+      </div>
+      <text1></text1>
+    </div>
+  );
+};
